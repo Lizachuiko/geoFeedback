9825b5512d1e02e6ca7856e8a6d9ebb04976ae59
Lizachuiko <33089704+Lizachuiko@users.noreply.github.com>
2018-02-26 16:43:49 +0300

    Update main.js

---
+++ src/scripts/main.js
@@ -0,0 +1,12 @@
+
+jQuery( document ).ready(function(){
+	if( 'devicePixelRatio' in window && window.devicePixelRatio > 1.5 ){
+	  var img_to_replace = jQuery( '#static-page-content img' ).get();
+
+	  for (var i=0,l=img_to_replace.length; i<l; i++) {
+		var src = img_to_replace[i].src;
+		src = src.replace(/\.(png|jpg|gif)+$/i, '@2x.$1');
+		img_to_replace[i].src = src;
+	  };
+	}
+  });
