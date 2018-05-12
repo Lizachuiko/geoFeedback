2c04028bcf21cc2652b533985abfea8f0f45f23d
chuiko <chuiko@MBP-RG.local>
2018-05-12 19:54:41 +0300

    add static content

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,36 @@
 
-jQuery( document ).ready(function(){
-	if( 'devicePixelRatio' in window && window.devicePixelRatio > 1 ){
-	  var img_to_replace = jQuery( '#static-page-content img' ).get();
+// function initMap() {  // why not work?
+window.initMap = function() {
+	var uluru = {lat: 55.753215, lng: 37.622504};
+	var map = new google.maps.Map(document.getElementById('map'), 
+	
+	{
+		zoom: 13.5,
+		center: uluru
+	});
 
-	  for (var i=0,l=img_to_replace.length; i<l; i++) {
-		var src = img_to_replace[i].src;
-		src = src.replace(/\.(png|jpg|gif)+$/i, '@2x.$1');
-		img_to_replace[i].src = src;
-	  };
-	}
-  });
+	var image = 'img/marker.png';
+
+	var markers = locations.map(function(location, i) {
+		return new google.maps.Marker({
+			position: location,
+			icon: image
+		});
+	});
+
+        // Add a marker clusterer to manage the markers.
+        var markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+      }
+      var locations = [
+        {lat: 55.757096, lng: 37.627176},
+        {lat: 55.752358, lng: 37.637733},
+      ]
+
+
+
+			// google.maps.event.addListener(marker, 'click', function() {
+			// 	marker.setIcon("img/markerRed.png");
+			// 	infowindow.open(map);
+			// 	});
+
